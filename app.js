99ffa872f68ed926e400ce41ea05f03e9c9682bc
Saqib <tnluser@Ananthanarayanans-MacBook-Pro.local>
2024-03-16 22:58:59 +0530

    added cluster module for scaling

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
-const express = require("express");
+const cluster = require('cluster');
+const os = require('os');
+const express = require('express');
 const dotenv=require('dotenv')
 const morgan= require('morgan')
 const cors = require("cors");
 const routes = require("./routes/index");
 const https = require('https');
 
+
 dotenv.config({ path: './config.env' });
 
-const app = express();
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-  ? process.env.ALLOWED_ORIGINS.split(',')
-  : [];
-app.use(cors({ origin: allowedOrigins }));
-app.use(morgan("dev"))
-app.use(express.json());
+const totalCpus = os.cpus().length;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ status: 'OK', message: 'Health check passed' });
-});
+    if(cluster.isPrimary){
+        for(let i = 0; i < totalCpus; i++){
+            cluster.fork();
+        }
+      } else{
+            
+            const app = express();
+            const allowedOrigins = process.env.ALLOWED_ORIGINS
+                                   ? process.env.ALLOWED_ORIGINS.split(',')
+                                   : [];
+            app.use(cors({ origin: allowedOrigins }));
+            app.use(morgan("dev"))
+            app.use(express.json());
 
-app.use("/", routes);
+            app.get('/', (req, res) => {
+            res.status(200).json({ status: 'OK', message: `Health check passed ${process.pid}` });
+            });
 
-setInterval(() => {
-  https.get(process.env.base_url);
-},5 * 60 * 1000);
+            app.use("/", routes);
 
+            setInterval(() => {
+            https.get(process.env.base_url);
+            },5 * 60 * 1000);
 
-const port = 4000;
+            const port = 4000;
+            app.listen(port, () => console.log(`server listening at port ${port}`));
 
-
-
-app.listen(port, () => console.log(`server listening at port ${port}....`));
+          }
